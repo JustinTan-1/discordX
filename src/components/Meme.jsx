396cbf9259d21233811a683b8d4198e99d2a6d40
Justin Tan <tanjustin217@gmail.com>
2024-10-29 09:13:56 -0600

    Added input and main content to page

---
+++ src/components/Meme.jsx
@@ -0,0 +1,16 @@
+import react from "react"
+
+export default function Meme() {
+    return (
+        <main>
+            <form className="form">
+                <input type="text"
+                className="form--input" placeholder="Top Text"/>
+                <input type="text"
+                className="form--input"
+                placeholder="Bottom Text"/>
+                <button className="form--button">Generate Meme</button>
+            </form>
+        </main>
+    )
+}
