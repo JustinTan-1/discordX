bf93fcd85d053154d3312dedd689ef60f3189ebc
Justin Tan <tanjustin217@gmail.com>
2024-10-30 13:17:58 -0600

    Added functionality to text inputs
    Typing text into the inputs now places meme text onto the image
    using a state object

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -2,12 +2,22 @@ import React from "react"
 import memesData from "../memesData.js"
 
 export default function Meme() {
-    const [allMemeImages, setMemData] = React.useState(memesData.data.memes)
-    const [meme, setImage] = React.useState({topText: "", bottomText: "", randomImage: "http://i.imgflip.com/1bij.jpg"})
+    const [allMemeImages, setMemeImagesData] = React.useState(memesData.data.memes)
+    const [memeData, setMemeData] = React.useState({topText: "", bottomText: "", randomImage: "http://i.imgflip.com/1bij.jpg"})
+
+    function handleChange(event) {
+        const {name, value} = event.target
+        setMemeData(prevMemeData => {
+            return {
+                ...prevMemeData,
+                [name]: value
+            }
+        })
+    }
 
     function getMeme() {
         const url = allMemeImages[Math.floor(Math.random()*allMemeImages.length)].url
-        setImage(prevMeme => {
+        setMemeData(prevMeme => {
             return ({...prevMeme, randomImage: url})
         })
     }
@@ -16,16 +26,24 @@ export default function Meme() {
             <div className="form">
                 <label> Top Text
                 <input type="text"
-                className="form--input" placeholder="When you discover"/>
+                className="form--input" placeholder="When you discover" onChange={handleChange}
+                name = "topText"
+                value = {memeData.topText}/>
                 </label>
                 <label> Bottom Text
                 <input type="text"
                 className="form--input"
-                placeholder="Meme Generator"/>
+                placeholder="Meme Generator" onChange={handleChange}
+                name = "bottomText"
+                value = {memeData.bottomText}/>
                 </label>
                 <button className="form--button" onClick={getMeme}>Generate Meme</button>
             </div>
-            <img src={meme.randomImage} className="meme--image"/>
+            <div className="meme">
+                <img src={memeData.randomImage} className="meme--image" />
+                <h2 className="meme--text top">{memeData.topText}</h2>
+                <h2 className="meme--text bottom">{memeData.bottomText}</h2>
+            </div>
         </main>
     )
 }
