f4186255e91fa20a82950cc3097b1e9afc85c512
Justin Tan <tanjustin217@gmail.com>
2024-10-31 10:49:45 -0600

    Improved header CSS styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,9 @@ import { Link } from "react-router-dom"
 export default function() {
     return( 
         <div className = "header">
+            <Link to="/" className = "header--text">
             <img src = {troll} className="header--img"/>
+            </Link>
             <Link to="/" className = "header--text">Meme Generator</Link>
             <Link to="/meme" className = "header--link">Static Memes</Link>
             <Link to="/gif" className = "header--link">Gifs</Link>
