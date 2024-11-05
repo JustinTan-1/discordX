4205a152e810a97741723f72288dc7cd552a6170
Justin Tan <tanjustin217@gmail.com>
2024-11-05 10:11:26 -0700

    Implemented scroll animation for homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,14 +9,24 @@ export default function Home() {
         <Header />
         <motion.div variants={reveal} initial="hiddenVariant" animate="revealedVariant" transition={{
             ease: "easeIn",
-            type:"tween",
+            type: "tween",
             staggerChildren: 0.1,
             duration: 0.5}} className="home--content">
-        <video autoPlay={true} loop={true} className="home--video" >
+            <h1 className="home--text">Welcome to Meme Generator</h1>
+        <div className="video--container">
+        <video autoPlay={true} loop={true} className="home--videos vid1" >
             <source src="https://media.tenor.com/vTjgtOd-evkAAAPw/torture-dance-jojo.mp4"/>
         </video>
-        <h1 className="home--text">Welcome to Meme Generator</h1>
-        <img src= { Frog } className="pepe"/>
+        <video autoPlay={true} loop={true} className="home--videos vid2" >
+            <source src="https://media.tenor.com/vTjgtOd-evkAAAPw/torture-dance-jojo.mp4"/>
+        </video>
+        <video autoPlay={true} loop={true} className="home--videos vid3" >
+            <source src="https://media.tenor.com/vTjgtOd-evkAAAPw/torture-dance-jojo.mp4"/>
+        </video>
+        <video autoPlay={true} loop={true} className="home--videos vid4" >
+            <source src="https://media.tenor.com/vTjgtOd-evkAAAPw/torture-dance-jojo.mp4"/>
+        </video>
+        </div>
         </motion.div>
         </div>
     )
